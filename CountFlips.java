4fc955f3ff916e395981c2a6ad6bf675be61f5fc
CarlTheOdd <carl31415@outlook.com>
2025-10-01 16:05:34 -0400

    Program 1 completed

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,20 @@
 public class CountFlips {
   public static void main(String[] args) {
+    int timesFlippedHeads = 0;
+    int timesFlippedTails = 0;
+
+    Coin coin = new Coin();
+
+    for(int i = 0; i < 100; i++) {
+      coin.flip();
+      
+      if(coin.isHeads()) {
+        timesFlippedHeads++;
+      } else {
+        timesFlippedTails++;
+      }
+    }
+
+    System.out.println("Heads was flipped " + timesFlippedHeads + " times, and tails was flipped " + timesFlippedTails + " times.");
   }
 }
